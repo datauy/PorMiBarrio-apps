e6c522a7a1ad578570b53eec303a0020ab2ef889
litoxperaloca <pablo.pignolo@gmail.com>
2017-05-20 14:00:48 -0300

    Se apunta a servidor de producción de Uruguay

--- src/js/services/config.services.js
+++ src/js/services/config.services.js
@@ -1,7 +1,7 @@
 pmb_im.services.factory('ConfigService', ['$http', function($http) {
 
   var ConfigObj = {};
-  ConfigObj.baseURL = "http://pmbdev.development.datauy.org";
+  ConfigObj.baseURL = "https://pormibarrio.uy";
   ConfigObj.baseCobrand = "/cobrands/pormibarrio";
 
   return ConfigObj;
